5fb3e34bf0cbf682b8b3170df356d4e403398087
merrettmi <mike.merrett@whitehorse.ca>
2025-11-30 00:07:30 -0500

    working on the decorator log_decorator

--- examples/basic_setup.py
+++ examples/basic_setup.py
@@ -10,22 +10,36 @@ basic_setup.py
 """
 __version__ = "0.0.0.0036"
 __author__ = "Mike Merrett"
-__updated__ = "2025-11-29 23:18:00"
+__updated__ = "2025-11-29 23:58:30"
 ###############################################################################
 
 
 import sys
 import os
+
+from MikesToolsLibrary.MyLogging import log_decorator
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 import logging
 import MikesToolsLibrary.MyLogging as mylog
-# from MikesToolsLibrary.MyLogging.CustomLevels import CustomLevels
-# from MikesToolsLibrary.MyLogging.CustomFormatter import CustomFormatter
 from MikesToolsLibrary.MyLogging.CustomFormatter import CustomFormatter, FormatMode
+from MikesToolsLibrary.MyLogging.log_decorator import log_decorator,log_decoratorPlain
 
+# import pprint
 
-import pprint
+@log_decorator
+def freddy( a, b,c):
+    print (a)
+    print(f"{b}")
+    print (f"{c=}")
+    return a+ b 
+
+@log_decoratorPlain
+def freddy2( a, b,c):
+    print (a)
+    print(f"{b}")
+    print (f"{c=}")
+    return a+ b 
 
 # Initialize unified logger
 logger = mylog.LoggerSetup("MikesToolsLibrary", level=logging.DEBUG, logfile=f"MikesToolsLibrary.log").get_logger()
@@ -43,6 +57,7 @@ mylog.LoggerSetup.add_level("NOTICE", 15, "\x1b[1;35;40m" , "‼")
 mylog.LoggerSetup.add_special_levels(logger)
 
 # Log messages
+logger.blkonyk("blkonyk message here")
 logger.debug("Debugging details")
 logger.info("Info message")
 logger.warning("Warning message")
@@ -50,16 +65,20 @@ logger.error("Error occurred")
 logger.critical("Critical issue")
 logger.notice("This is a NOTICE message")
 
-logger.setLevel(1)
+logger.setLevel(0)
+logger.debug("This debug message will be filtered out")
 # Exclude DEBUG logs from console/file
 # setup.add_filter(logging.DEBUG)
-logger.debug("This debug message will be filtered out")
 
 logger.tracez("zzzzzzzzzzzzzzzz")
-logger.blkonyk("blkonyk message here")
 
 #mylog.LoggerSetup.show_all_levels(logger, False)
 # mylog.LoggerSetup.show_all_levels(logger, True)
+
+
+x = freddy("sam was here ", "tom is gone", 777)
+y = freddy2("sam was herexxx ", "tom is gonexxxxxxxx", 87778)
+
 
 
 alist = ["aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb", "ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc", "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd", "e"]
